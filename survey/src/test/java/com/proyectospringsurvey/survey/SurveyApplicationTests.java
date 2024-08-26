aa5e8c0cfff19052a591816e0889f8e0c925b96e
camih1005 <camiloht0918@gmail.com>
2024-08-26 14:42:34 -0500

    eje

---
+++ survey/src/test/java/com/proyectospringsurvey/survey/SurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectospringsurvey.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
