7e96ab0df7ea59cc4cdb1af3d9971cbd19bd88f1
christiancelis <cristian_celis10@hotmail.com>
2024-08-30 19:54:41 -0500

    cors

--- survey/src/main/java/com/proyectospringsurvey/survey/surveysCategory/infrastructure/ControllersCategory.java
+++ survey/src/main/java/com/proyectospringsurvey/survey/surveysCategory/infrastructure/ControllersCategory.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +22,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/")
+@CrossOrigin(origins = "*")
 public class ControllersCategory {
 
     @Autowired
     private ImpServicesCategory impServicesCategory;
+
 
     @PostMapping("SurveyCategory")
     public ResponseEntity<?> createsCategory(@RequestBody Optional<surveysCategory> sCategory) {
