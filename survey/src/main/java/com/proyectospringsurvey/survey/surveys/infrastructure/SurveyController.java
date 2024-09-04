434a628ff9d10277866517757c1824af7f0aaa45
christiancelis <73555661+christiancelis@users.noreply.github.com>
2024-09-04 11:25:24 -0500

    cors for survey

--- survey/src/main/java/com/proyectospringsurvey/survey/surveys/infrastructure/SurveyController.java
+++ survey/src/main/java/com/proyectospringsurvey/survey/surveys/infrastructure/SurveyController.java
@@ -17,7 +17,6 @@ import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +27,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/api/")
-@CrossOrigin(origins = "*")
 public class SurveyController {
     @Autowired
     private SurveyImpService surveyImpService;
