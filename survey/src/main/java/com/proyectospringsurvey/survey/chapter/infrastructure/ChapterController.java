b71bf0c5b58b814d4b812efd27d4dcb15cedbf75
christiancelis <73555661+christiancelis@users.noreply.github.com>
2024-09-04 11:28:52 -0500

    all cors

--- survey/src/main/java/com/proyectospringsurvey/survey/chapter/infrastructure/ChapterController.java
+++ survey/src/main/java/com/proyectospringsurvey/survey/chapter/infrastructure/ChapterController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/")
-@CrossOrigin(origins = "*")
 public class ChapterController {
 
     @Autowired 
--- survey/src/main/java/com/proyectospringsurvey/survey/responseOptions/infrastructure/ResponseOptionsController.java
+++ survey/src/main/java/com/proyectospringsurvey/survey/responseOptions/infrastructure/ResponseOptionsController.java
@@ -16,7 +16,6 @@ import com.proyectospringsurvey.survey.responseOptions.domain.ResponseOptions;
 
 @RestController
 @RequestMapping("/api/")
-@CrossOrigin(origins = "*")
 public class ResponseOptionsController {
 
     @Autowired
